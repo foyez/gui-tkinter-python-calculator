8cda7fdcdfa8b0950ce7ca2612b7fc6f126da46a
foyezar <foyezar@yahoo.com>
2019-08-02 17:14:00 +0600

    fill

--- calculator.py
+++ calculator.py
@@ -3,16 +3,15 @@ import tkinter
 window = tkinter.Tk()
 window.title('Calculator')
 
-top_frame = tkinter.Frame(window).pack()
-bottom_frame = tkinter.Frame(window).pack(side='bottom')
+# sufficient width
+tkinter.Label(window, text='Suf. width', fg='white', bg='purple').pack()
 
-btn1 = tkinter.Button(top_frame, text='Button1', fg='red').pack()
-btn2 = tkinter.Button(top_frame, text='Button2', fg='green').pack()
-btn3 = tkinter.Button(bottom_frame, text='Button3',
-                      fg='purple').pack(side='left')
-btn4 = tkinter.Button(bottom_frame, text='Button4',
-                      fg='orange').pack(side='left')
+# width of X
+tkinter.Label(window, text='Taking all available X width',
+              fg='white', bg='green').pack(fill='x')
 
-# label = tkinter.Label(window, text='Hello world').pack()
+# height of Y
+tkinter.Label(window, text='Taking all available Y height',
+              fg='white', bg='black').pack(side='right', fill='y')
 
 window.mainloop()
