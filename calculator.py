9f9d69a1cc67af51ec649f7d5f40f727be2da51a
foyezar <foyezar@yahoo.com>
2019-08-07 12:30:13 +0600

    Calculator

--- calculator.py
+++ calculator.py
@@ -1,27 +1,99 @@
-import tkinter
-import tkinter.messagebox
+from tkinter import *
 
 
-window = tkinter.Tk()
-window.title('GUI')
+window = Tk()
+# size of the window width:- 500, height:- 375
+window.geometry('312x324')
+# this prevents from resizing the window
+window.resizable(0, 0)
+window.title('Calculator')
+
+# 'btn_click' function continuously updates the input field whenever you enters a number
 
 
-def function():
-    pass
+def btn_click(item):
+    global exp
+    exp = exp + str(item)
+    input_text.set(exp)
 
 
-# creating a root menu to insert all the sub menu
-root_menu = tkinter.Menu(window)
-window.config(menu=root_menu)
+# 'btn_equal' calculates the expression present in input field
+def btn_clear():
+    global exp
+    exp = ''
+    input_text.set('')
 
-# creating the 'Canvas' area of width and height 500px
-canvas = tkinter.Canvas(window, width=500, height=200)
-canvas.pack()
+# 'btn_equal' calculates the expression present in input field
 
-# taking image from the directory and storing the source in a variable
-icon = tkinter.PhotoImage(file='img/linux.png')
-# displaying the picture using a 'Label' by passing the 'picture' variable to 'image' parameter
-label = tkinter.Label(window, image=icon)
-label.pack()
+
+def btn_equal():
+    global exp
+    # 'eval' function evalutes the string expression directly
+    # you can also implement your own function to evalute the expression istead of 'eval' function
+    result = str(eval(exp))
+    input_text.set(result)
+    exp = ''
+
+
+exp = ''
+# 'StringVar()' is used to get the instance of input field
+input_text = StringVar()
+
+
+# creating a frame for the input field
+input_frame = Frame(window, width=312, height=50, bd=0,
+                    highlightbackground="black", highlightcolor="black", highlightthickness=1)
+input_frame.pack(side=TOP)
+
+
+# creating a input field inside the 'Frame'
+input_filed = Entry(input_frame, font=(
+    'arial', 18, 'bold'), textvariable=input_text, width=50, bg='#eee', bd=0, justify=RIGHT)
+input_filed.grid(row=0, column=0)
+# 'ipady' is internal padding to increase the height of input field
+input_filed.pack(ipady=10)
+
+
+# creating another 'Frame' for the button below the 'input_frame'
+btns_frame = Frame(window, width=312, height=272.5, bg='grey')
+btns_frame.pack()
+
+
+# first row
+clear = Button(btns_frame, text='C', fg='black', width=32, height=3,
+               bd=0, bg='#eee', cursor='hand2', command=lambda: btn_clear()).grid(row=0, column=0, columnspan=3, padx=1, pady=1)
+divide = Button(btns_frame, text='/', fg='black', width=10, height=3, bd=0, bg='#eee',
+                cursor='hand2', command=lambda: btn_click('/')).grid(row=0, column=3, padx=1, pady=1)
+
+
+# second row
+seven = Button(btns_frame, text='7', fg='black', width=10, height=3, bd=0, bg='#fff',
+               cursor='hand2', command=lambda: btn_click(7)).grid(row=1, column=0, padx=1, pady=1)
+eight = Button(btns_frame, text='8', fg='black', width=10, height=3, bd=0,
+               bg='#fff', cursor='hand2', command=lambda: btn_click(8)).grid(row=1, column=1, padx=1, pady=1)
+nine = Button(btns_frame, text='9', fg='black', width=10, height=3, bd=0, bg='#fff',
+              cursor='hand2', command=lambda: btn_click(9)).grid(row=1, column=2, padx=1, pady=1)
+multiply = Button(btns_frame, text='*', fg='black', width=10, height=3, bd=0, bg='#fff',
+                  cursor='hand2', command=lambda: btn_click('*')).grid(row=1, column=3, padx=1, pady=1)
+
+
+# third row
+four = Button(btns_frame, text = "4", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(4)).grid(row = 2, column = 0, padx = 1, pady = 1)
+five = Button(btns_frame, text = "5", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(5)).grid(row = 2, column = 1, padx = 1, pady = 1)
+six = Button(btns_frame, text = "6", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(6)).grid(row = 2, column = 2, padx = 1, pady = 1)
+minus = Button(btns_frame, text = "-", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("-")).grid(row = 2, column = 3, padx = 1, pady = 1)
+
+
+# fourth row
+one = Button(btns_frame, text = "1", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(1)).grid(row = 3, column = 0, padx = 1, pady = 1)
+two = Button(btns_frame, text = "2", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(2)).grid(row = 3, column = 1, padx = 1, pady = 1)
+three = Button(btns_frame, text = "3", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(3)).grid(row = 3, column = 2, padx = 1, pady = 1)
+plus = Button(btns_frame, text = "+", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("+")).grid(row = 3, column = 3, padx = 1, pady = 1)
+
+
+# fifth row
+zero = Button(btns_frame, text='0', fg='black', width=21, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: btn_click(0)).grid(row=4, column=0, columnspan=2, padx=1, pady=1)
+point = Button(btns_frame, text = ".", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click(".")).grid(row = 4, column = 2, padx = 1, pady = 1)
+equals = Button(btns_frame, text = "=", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_equal()).grid(row = 4, column = 3, padx = 1, pady = 1)
 
 window.mainloop()
